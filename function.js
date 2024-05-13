9a8850d753082c23e77bd4870469298bf679b4d9
kazipintu <kazipintu@gmail.com>
2024-05-13 22:57:21 +0530

    MERN/DevZone/JS/function

---
+++ function.js
@@ -0,0 +1,107 @@
+// FUNCTION //
+
+{
+  function func1(params) {
+    console.log('abul ' + 'habul');
+  }
+  func1();
+}
+
+{
+  let firstName = 'Afdal';
+  let lastName = 'is a bad student'
+  function fun2() {
+    console.log(firstName + ' ' + lastName);
+  }
+  fun2()
+}
+
+{
+  let x = 2;
+  let y = 3;
+  function fun3() {
+    if (y > x) {
+      console.log('y is greater than x: ' + y);
+    }
+    else {
+      console.log('error');
+    }
+  }
+  fun3();
+}
+
+{
+  let first = 'Afdal';
+  let last = 'is a bad student'
+  function fun4() {
+    let statement = first + ' ' + last;
+    console.log(statement);
+  }
+  fun4();
+}
+
+{
+  let a = 'mushfiq';
+  let b = 4;
+  function fun5() {
+    let output = a + b;
+    console.log(output);
+
+  }
+  fun5()
+}
+
+{
+  let a = '50';
+  let b = 4;
+  function fun6() {
+    let output = a + b;
+    console.log(output);
+
+  }
+  fun6()
+}
+
+{
+  let a = '50';
+  let b = '4';
+  function fun7() {
+    let output = a + b;
+    console.log(output);
+
+  }
+  fun7()
+}
+
+{
+  let a = 50;
+  let b = '4';
+  function fun7() {
+    let output = a + b;
+    console.log(output);
+
+  }
+  fun7()
+}
+
+{
+  let a = 50;
+  let b = -'4';
+  function fun7() {
+    let output = a + b;
+    console.log(output);
+
+  }
+  fun7()
+}
+
+{
+
+  function fun8(a, b) {
+    let name1 = a + ' ' + b;
+    return console.log(name1);
+
+  }
+  fun8('Kazi', 'Afzaluddin')
+}
+
